697d346d7f3463f737932a33639a75cdd6b6496d
tunkhine126 <tunkhine126@gmail.com>
2019-05-17 17:16:26 -0400

    Done

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :seas
+
+  # destroy '/seas/:id(.:format)'
+
+  # get '/seas/:id/destroy' => 'seas#destroy'
+
+    get '/' => 'application#welcome'
 
 end
