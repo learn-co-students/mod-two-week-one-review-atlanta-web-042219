19901c390882fff3fbb9802243d0683794c13eaf
ascotttoney <ascotttoney@gmail.com>
2019-05-17 16:57:03 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,48 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
 
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.new(sea_params)
+    if @sea.valid?
+      @sea.save
+      redirect_to sea_path(@sea)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    if @sea.update_attributes(sea_params)
+      redirect_to sea_path(@sea)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    if @sea.destroy
+      redirect_to '/seas'
+    else
+      render :edit
+    end
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
